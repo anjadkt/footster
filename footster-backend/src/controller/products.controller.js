e344350a9776786cb4acd46b206c49c3ddd0dd40
anjadkt <anjad076@gmail.com>
2025-11-17 20:01:54 +0530

    update product api setup

--- footster-backend/src/controller/products.controller.js
+++ footster-backend/src/controller/products.controller.js
@@ -1,20 +1,25 @@
 const Products = require('../model/products.model.js');
 module.exports = {
   getProducts : async (req,res)=>{
-                const {_page , _limit} = req.query ;
-                
-                const limit = Number(_limit);
-                const page = ( ( Number(_page) -1 ) * limit );
-                
-                try{
-                  const products = await Products.aggregate([
-                    {$skip : page},
-                    {$limit : limit }
-                  ]);
-                  res.json(products);
-                }catch(error){
-                  console.log(error.message);
-                }
-                
-              }
+    if(req.query){
+      const {_page , _limit} = req.query ;
+      
+      const limit = Number(_limit);
+      const page = ( ( Number(_page) -1 ) * limit );
+      
+      try{
+        const products = await Products.aggregate([
+          {$skip : page},
+          {$limit : limit }
+        ]);
+        res.json(products);
+      }catch(error){
+        console.log(error.message);
+      }
+    }else{
+      const products = await Products.find();
+      res.json(products);
+    }
+     
+  }
 }
