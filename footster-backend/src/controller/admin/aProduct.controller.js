de271557c194dfaf5d9421f12abe04f32c8ec043
anjadkt <anjad076@gmail.com>
2025-11-21 12:13:32 +0530

    add api for product updation

--- footster-backend/src/controller/admin/aProduct.controller.js
+++ footster-backend/src/controller/admin/aProduct.controller.js
@@ -23,5 +23,44 @@ module.exports = {
     } catch (error) {
       res.status(500).json({message : error.message , status : 500 });
     }
+  },
+  removeProduct : async (req,res)=>{
+    try {
+      const {id} = req.body ;
+      const product = await Product.findOne({_id : id});
+      if(!product)res.status(404).json({message : "Product not found!",status :404});
+
+      const deletedProduct = await Product.deleteOne({_id : id});
+
+      res.status(200).json({
+        message : "Product removed!",
+        status : 200,
+        deletedProduct
+      })
+
+    } catch (error) {
+      res.status(500).json({message : error.message , status : 500});
+    }
+  },
+  updateProduct : async (req,res)=>{
+    try {
+      const {id , img , category , price , name } = req.body ;
+      if(!id || !img || !category || !price ||!name)return res.status(400).json({message : "incomplete form!",status:400});
+
+      const updatedProduct = await Product.updateOne({_id : id} , {
+        name,
+        category,
+        price,
+        img
+      });
+
+      res.status(200).json({
+        message : "Product Updated!",
+        updatedProduct,
+        status : 200
+      })
+    } catch (error) {
+      res.status(500).json({message : error.message , status : 500});
+    }
   }
 }
--- footster-backend/src/router/admin/aProduct.route.js
+++ footster-backend/src/router/admin/aProduct.route.js
@@ -5,5 +5,7 @@ const adminProductController = require('../../controller/admin/aProduct.controll
 
 router.get('/all',productController.getAllProducts);
 router.post('/add',adminProductController.addProduct);
+router.delete('/remove',adminProductController.removeProduct);
+router.put('/update',adminProductController.updateProduct);
 
 module.exports = router
